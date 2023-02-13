4df007a6708103b74daf7a01a1909d4b792d19a7
Oleksandr Yushchenko <sanyeks@gmail.com>
2023-02-13 20:14:02 +0200

    add new changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './css/styles.css';
+import debounce from "lodash.debounce";
+import {fetchCountries} from './fetchCountries.js'
+
+const refs = {
+    countryInfo : document.querySelector('.country-info'),
+    countryList: document.querySelector('.country-list'),
+    input : document.getElementById('search-box')
+}
 
 const DEBOUNCE_DELAY = 300;
+
+refs.input.addEventListener('input',debounce(onSearch,DEBOUNCE_DELAY))
+
+function onSearch(e) {
+let name = e.target.value;
+let NAME = name.trim().toLowerCase();
+if(NAME === ''){
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    return;
+}
+fetchCountries(NAME)
+
+}
