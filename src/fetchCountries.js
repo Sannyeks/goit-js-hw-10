8c84ccffe3873cb1d0dad1cddc62940c92fff5d7
Oleksandr Yushchenko <sanyeks@gmail.com>
2023-02-18 15:42:24 +0200

    add new files

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -7,52 +7,55 @@ const refs = {
 }
 
 export function fetchCountries(name) {
-    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status)
+ fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+ .then(response => {
+ if (!response.ok) {
+throw new Error(response.status)
             }
             return response.json()
-        })
-        .then(data => {
-            const oneMatch = data.length === 1;
-            const maxMatch = 10;
-            if (oneMatch) {
-                const markup = data.map(country =>
-                    `<li class="country-item">
-               <div class="country-item__info">
-               <img class="country-item__flag"src="${country.flags.svg}" alt="flag ${country.name.official}">
-               <h1 class="country-item__name">${country.name.official}</h1>
-               </div>
-               <p class="country-item__capital">Capital :${country.capital}</p>
-               <p class="country-item__population">Population :${country.population}</p>
-               <p class="country-item__languages">Languages :${Object.values(country.languages)}</p>
-               </li>
-               `).join('')
-                refs.countryList.innerHTML = markup;
-            }
-            else if (data.length <= maxMatch) {
-                const markup = data.map(country =>
-                    `<div class="country-item__info">
-                  <img class="country-item__flag"src="${country.flags.svg}" alt="flag ${country.name.official}">
-                   <h1 class="country-item__name">${country.name.official}</h1>
-                   </div>
-               `).join('')
-                refs.countryList.innerHTML = "";
-                refs.countryInfo.innerHTML = markup;
-            }
-            else if (data.length > maxMatch) {
-                refs.countryList.innerHTML = '';
-                refs.countryInfo.innerHTML = '';
-                Notify.info("To many matches found. Please enter a more specific name");
-            }
-        })
-        .catch(error => {
-            if (error.message === '404') {
-                refs.countryList.innerHTML = '';
-                refs.countryInfo.innerHTML = '';
-                Notify.failure('Oops, there is no country with that name');
-            }
-            console.log(error);
-        })
+ })
+     
+.then(data => {
+const oneMatch = data.length === 1;
+const maxMatch = 10;
+if (oneMatch) {
+const markup = data.map(country =>
+ `<li class="country-item">
+ <div class="country-item__info">
+<img class="country-item__flag"src="${country.flags.svg}" alt="flag ${country.name.official}">
+<h1 class="country-item__name">${country.name.official}</h1>
+</div>
+<p class="country-item__capital">Capital :${country.capital}</p>
+<p class="country-item__population">Population :${country.population}</p>
+<p class="country-item__languages">Languages :${Object.values(country.languages)}</p>
+</li>
+`).join('')
+refs.countryList.innerHTML = markup;
+}
+    
+else if (data.length <= maxMatch) {
+const markup = data.map(country =>
+ `<div class="country-item__info">
+ <img class="country-item__flag"src="${country.flags.svg}" alt="flag ${country.name.official}">
+ <h1 class="country-item__name">${country.name.official}</h1>
+ </div>
+    `).join('')
+    refs.countryList.innerHTML = "";
+    refs.countryInfo.innerHTML = markup;
     }
+    else if (data.length > maxMatch) {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    Notify.info("To many matches found. Please enter a more specific name");
+    }
+})
+     
+.catch(error => {
+if (error.message === '404') {
+refs.countryList.innerHTML = '';
+refs.countryInfo.innerHTML = '';
+Notify.failure('Oops, there is no country with that name');
+}
+console.log(error);
+})
+}
